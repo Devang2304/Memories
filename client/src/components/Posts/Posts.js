5d78efbe17fb557a1ccf8e2e49d6853de0a10967
Devang2304 <devangvartak2304@gmail.com>
2022-10-07 00:26:21 +0530

    Creating and structuring JSX files

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const Posts=()=>{
+  return (
+    <><h1>POSTS</h1>
+    
+    </> // <></> this is calle das react fragment so that we can create lots of more things in this
+    );
+}
+
+export default Posts;

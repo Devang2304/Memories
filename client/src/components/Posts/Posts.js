a92004f972d830810b0b4c4d0476414107715e38
Devang2304 <devangvartak2304@gmail.com>
2022-10-07 01:44:39 +0530

    Added style component material Ui is not rendering

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -3,8 +3,10 @@ import Post from './Post/Post';
 
 const Posts=()=>{
   return (
-    <><h1>POSTS</h1>
-    
+    <>
+    <h1>POSTS</h1>
+    <Post/>
+    <Post/>
     </> // <></> this is calle das react fragment so that we can create lots of more things in this
     );
 }

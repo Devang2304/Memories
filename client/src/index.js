880764676092d03f1339a446701207346d83c88d
Devang2304 <devangvartak2304@gmail.com>
2023-01-12 12:27:30 +0530

    redesigning the project again

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root')); // Through this line we are connecting div with an id of the div
-

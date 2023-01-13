d1bb885d07389bacee28a71158aacb18d8b9570f
Devang2304 <devangvartak2304@gmail.com>
2023-01-13 19:24:18 +0530

    created required file structure and NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {Container,AppBar,typography,Grow,Grid, Typography} from '@mui/material'
+import memories from './images/memories.png'
 
 const App=()=> {
   return (
-    <div>
-      <h1>You are on home</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position='static' color='inherit'>
+        <Typography varient="h2" align='center'>Memories</Typography>
+        <img src={memories} alt="memories" height="60"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                  <Posts/>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                  <Form />
+              </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 

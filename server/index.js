92c1ad3759ac85063516797ec90d8edf1a974d83
Devang2304 <devangvartak2304@gmail.com>
2023-01-13 17:38:48 +0530

    created server and imported all the require mdoules

--- server/index.js
+++ server/index.js
@@ -2,10 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import postRoutes from './routes/posts.js'
 const app =express();
-PORT = 5000;
+app.use(bodyParser.json({limit:"30mb",extended :true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended :true}));
+app.use(cors());
+app.use('/posts',postRoutes);
+const PORT = 5000;
 
+const CONNECTION_URL = 'mongodb+srv://DEVANG23:sGS9G3qDgNVQztE@cluster0.ngb8lfk.mongodb.net/test';
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log('Connected to MongoDB successfully');
+}).catch((err)=>{console.log('No connection')});
 app.listen(PORT,()=>{
-    console.log(`Listening to port ${PORT}`);
+    console.log(`Listening to port http://localhost:${PORT}`);
 })
+
+app.get('/', (req, res)=>{
+    res.send('Home');
+});
+

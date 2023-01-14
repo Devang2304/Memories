3125cda50facb4b870d650d8b3c908e3d918b4ac
Devang2304 <devangvartak2304@gmail.com>
2023-01-14 20:49:58 +0530

    imported all the required modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {Container,AppBar,typography,Grow,Grid, Typography} from '@mui/material'
+import {Container,AppBar,Grow,Grid, Typography} from '@mui/material'
 import memories from './images/memories.png'
-
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form';
+import useStyles from './styles';
 const App=()=> {
+  const classes =useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position='static' color='inherit'>
-        <Typography varient="h2" align='center'>Memories</Typography>
-        <img src={memories} alt="memories" height="60"/>
+      <AppBar  className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>

01cedd95ca352594fa642a30d69b40cff057e3a2
Devang2304 <devangvartak2304@gmail.com>
2023-01-22 21:11:48 +0530

    added theme provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Form from './components/Form/Form';
+import {createTheme} from '@mui/material';
+import {ThemeProvider} from '@mui/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +12,12 @@ root.render(
   </React.StrictMode>
 );
 
+
+const Root = () => {
+const theme = createTheme()
+    return (
+        <ThemeProvider theme={theme}>
+            <Form/>
+        </ThemeProvider>
+ )}
+export default Root

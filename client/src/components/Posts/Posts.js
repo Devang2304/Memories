71abe0dfba3f5cff1b2095cb0d1d29288a2befe4
Devang2304 <devangvartak2304@gmail.com>
2023-03-15 23:28:47 +0530

    added loader & incoming mapping of the post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import {Grid,CircularProgress} from '@mui/material';
 
 import Post  from './Post/Post'
 import useStyles from './styles'
@@ -11,11 +12,19 @@ const Posts=()=>{
 
     console.log(posts);
     return (
-        <>
-        <h1>POSTS</h1>
-        <Post/>
-        <Post/>
-        </>
-    );
+        !posts.length ? <CircularProgress/> : (
+           <Grid className={classes.container} container alginItems="stretch" spacing={3}>
+            {posts.map((post)=>(
+                <Grid key={post._id} item xs={12} sm={6}>
+                    <Post post={post}/>
+                </Grid>
+            ))
+
+            }
+           </Grid>
+        )
+    ); 
 }
+    
+
  export default Posts;
